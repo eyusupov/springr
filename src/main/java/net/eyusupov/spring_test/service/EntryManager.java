d11bf6faceb909e77c173707fabbf44b700fec0f
Eldar Yusupov <eyusupov@gmail.com>
2013-02-18 00:17:32 +0400

    Added EntryManager and simple implementation for it

--- src/main/java/net/eyusupov/spring_test/service/EntryManager.java
+++ src/main/java/net/eyusupov/spring_test/service/EntryManager.java
@@ -4,10 +4,14 @@
  */
 package net.eyusupov.spring_test.service;
 
+import java.util.List;
+import net.eyusupov.spring_test.domain.Entry;
+
 /**
  *
  * @author eyusupov
  */
 public interface EntryManager {
-    
+    public List<Entry> getEntries();
+    public void setEntries(List<Entry> entries);
 }
